e4b82836f0b4c0152ec81ede84653cb56d342b80
imran688-dev <farabiahmedimran543@gmail.com>
2023-11-29 23:16:01 +0600

    Make a Basic Calculator

---
+++ DOM.js
@@ -0,0 +1,29 @@
+
+// let heading = document.getElementById("text");
+// console.log(heading);
+// heading.innerHTML = "Document Model";
+
+// document.getElementById("text").innerHTML = "you are fine finding location";
+
+// document.getElementById("text").style.color = "red";
+// document.getElementById("text").style.fontSize = "50px";
+// document.getElementById("text").style.backgroundColor = "black";
+// document.getElementById("text").style.padding = "30px 100px";
+
+
+
+// document.getElementsById("para").innerHTML = "I hope this text is most important because it's esential for us and their.";
+
+
+// document.getElementsByClassName("tag");
+
+
+let query = document.querySelector("body");
+console.log(query);
+
+let h3 = document.querySelector(".tag");
+h3.style.color = "red";
+
+
+
+
